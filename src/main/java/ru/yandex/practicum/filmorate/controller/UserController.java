40ec623b06b05cbe4c61d4e4cf82cce38c9b9954
unknown <eleonora.brueva@yandex.com>
2026-04-04 15:35:45 +0300

    Удален ненужный импорт из класса UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
